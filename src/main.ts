c641b009306971a24748348c378cb3af6ab8174f
Harry Hopkinson <harryjhopkinson@gmail.com>
2024-08-30 11:36:13 +0100

    Use stores for the variables

--- src/main.ts
+++ src/main.ts
@@ -8,43 +8,43 @@ import {
   window,
 } from "openrct2-flexui";
 
-let suggestedGuestMaximum: number = 0;
-let currentGuestMaximum: WritableStore<number> = store(
+let SuggestedMaxGuests: WritableStore<number> = store(0);
+let CurrentGuestMaximum: WritableStore<number> = store(
   park.suggestedGuestMaximum,
 );
-let activate: boolean = false;
-let event: any;
+let Activate: WritableStore<boolean> = store(false);
+let Event: any;
 
-export const allWidgets = window({
-  title: "OpenRCT2 Soft Guest Cap Editor",
+export const MainWindow = window({
+  title: "Soft Guest Cap Editor",
   width: { value: 300, min: 300, max: 350 },
   height: { value: 100, min: 100, max: 110 },
   content: [
     label({
       alignment: "centred",
-      text: `Enter your desired soft guest cap - Current: ${currentGuestMaximum.get()}`,
+      text: `Enter your desired soft guest cap`,
       tooltip:
         "This is the maximum number of guests that will be allowed in your park",
     }),
     textbox({
-      text: currentGuestMaximum.get().toString(),
+      text: CurrentGuestMaximum.get().toString(),
       onChange: (text: string) => {
-        suggestedGuestMaximum = Number(text);
+        SuggestedMaxGuests.set(Number(text));
       },
     }),
     checkbox({
       text: "Activate custom soft guest cap",
-      isChecked: activate,
+      isChecked: Activate.get(),
       onChange: function (checked) {
-        activate = checked;
+        Activate.set(checked);
         if (checked) {
-          event = context.subscribe("park.guest.softcap.calculate", (e) => {
-            e.suggestedGuestMaximum = suggestedGuestMaximum;
-            currentGuestMaximum.set(suggestedGuestMaximum);
+          Event = context.subscribe("park.guest.softcap.calculate", (e) => {
+            e.suggestedGuestMaximum = SuggestedMaxGuests.get();
+            CurrentGuestMaximum.set(SuggestedMaxGuests.get());
           });
-        } else if (event) {
-          event.dispose();
-          event = null;
+        } else if (Event) {
+          Event.dispose();
+          Event = null;
         }
       },
     }),
@@ -63,6 +63,6 @@ export const allWidgets = window({
 export function main() {
   if (typeof ui !== "undefined") {
     const menuItemName = "Soft Guest Cap Editor";
-    ui.registerMenuItem(menuItemName, () => allWidgets.open());
+    ui.registerMenuItem(menuItemName, () => MainWindow.open());
   }
 }
